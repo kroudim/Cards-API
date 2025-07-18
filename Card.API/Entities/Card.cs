096037bde86ba2bad60f7d5c1190e01beff9c0d6
kroudim <kroudim@gmail.com>
2025-07-18 22:49:51 +0300

    Add files via upload

---
+++ Card.API/Entities/Card.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class Card    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+
+        [MaxLength(200)]
+        [RegularExpression(@"^([a-zA-Z0-9])$")]
+        [StringLength(7,MinimumLength = 7)]
+        public string? Color { get; set; }
+
+        [MaxLength(200)]
+        public string? Status { get; set; }
+
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+        public int UserId { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public Card(string name, string description,string color, string status )
+        {
+            Name = name;
+            Description = description;
+            Color = color;
+            Status = status;
+            CreationTime = DateTime.Now;
+        }
+    }
+}
---
+++ Card.API/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class User    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [MaxLength(200)]
+        public string? Password { get; set; }
+
+
+        [MaxLength(200)]
+        public string? Role { get; set; }
+
+
+    public ICollection<Card> Cards { get; set; }
+               = new List<Card>();
+
+        public User(string email, string password, string role)
+        {
+         Email = email;
+         Password =password;
+         Role = role;
+        }
+    }
+}
