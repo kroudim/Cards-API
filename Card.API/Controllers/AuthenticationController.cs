5a91ef24a9718bd6bbb2848b26d3debbf3249306
kroudim <kroudim@gmail.com>
2025-07-18 22:49:21 +0300

    Add files via upload

---
+++ Card.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,77 @@
+﻿using CityInfo.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ICardRepository _cityInfoRepository;
+
+        // we won't use this outside of this class, so we can scope it to this namespace
+        public class AuthenticationRequestBody
+        {
+            public string? Email { get; set; }
+            public string? Password { get; set; }
+        }
+
+        public AuthenticationController(IConfiguration configuration, ICardRepository cityInfoRepository)
+        {
+            _configuration = configuration ?? 
+                throw new ArgumentNullException(nameof(configuration));
+            _cityInfoRepository = cityInfoRepository ??
+            throw new ArgumentNullException(nameof(cityInfoRepository));
+      }
+
+        [HttpPost("authenticate")]
+        public  ActionResult<string> Authenticate(
+            AuthenticationRequestBody authenticationRequestBody)
+        {  
+            // Step 1: validate the username/password
+            var user = ValidateUserCredentials(
+                authenticationRequestBody.Email,
+                authenticationRequestBody.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            // Step 2: create a token
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+             
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("userid", user.Id.ToString()));
+            claimsForToken.Add(new Claim("role", user.Role));
+             
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler()
+               .WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+
+        private User ValidateUserCredentials(string? userName, string? password)
+        {
+           return  _cityInfoRepository.GetCurrentUserAsync(userName, password).Result;
+        }
+    }
+}
---
+++ Card.API/Controllers/CardsController.cs
@@ -0,0 +1,148 @@
+﻿using AutoMapper;
+using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/cards")]
+    public class CardsController : ControllerBase
+    {
+        private readonly ICardRepository _cityInfoRepository;
+        private readonly IMapper _mapper;
+        const int maxCardsPageSize = 20;
+
+        public CardsController(ICardRepository cityInfoRepository,
+            IMapper mapper)
+        {
+            _cityInfoRepository = cityInfoRepository ?? 
+                throw new ArgumentNullException(nameof(cityInfoRepository));
+            _mapper = mapper ?? 
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CardDto>>> GetCards(
+            string? name, string? color, string? status, string? searchQuery, string? orderbyQuery, int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageSize > maxCardsPageSize)
+            {
+                pageSize = maxCardsPageSize;
+            }
+
+            var userId = GetUserId();
+            var (cityEntities, paginationMetadata) = await _cityInfoRepository
+                .GetCardsAsync(userId, name, color, status , searchQuery, orderbyQuery, pageNumber, pageSize);
+
+            Response.Headers.Add("X-Pagination",
+                JsonSerializer.Serialize(paginationMetadata));
+
+            return Ok(_mapper.Map<IEnumerable<CardDto>>(cityEntities));       
+        }
+
+        [HttpGet("{id}", Name = "GetCard")]
+        public async Task<IActionResult> GetCard(int id)
+        {
+            var userId = GetUserId();
+
+            var card = await _cityInfoRepository.GetCardAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            var isgranted = await _cityInfoRepository.UserHasAccessToCardAsync(id, userId);
+            if (!isgranted)
+            {
+                return Forbid();
+            }
+
+            return Ok(_mapper.Map<CardDto>(card));
+        }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateCard(int id, CardForUpdateDto card)
+      {
+      var userId = GetUserId();
+
+      var cardEntity = await _cityInfoRepository
+          .GetCardAsync(id);
+
+      if (cardEntity == null)
+        {
+        return NotFound();
+        }
+
+      var isgranted = await _cityInfoRepository.UserHasAccessToCardAsync(id, userId);
+      if (!isgranted)
+        {
+        return Forbid();
+        }
+
+      _mapper.Map(card, cardEntity);
+
+      await _cityInfoRepository.SaveChangesAsync();
+
+      return NoContent();
+      }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteCard(int id)
+      {
+      var userId = GetUserId();
+
+      if (!await _cityInfoRepository.CardExistsAsync(id))
+        {
+        return NotFound();
+        }
+
+      var isgranted = await _cityInfoRepository.UserHasAccessToCardAsync(id, userId);
+      if (!isgranted)
+        {
+        return Forbid();
+        }
+
+      var pointOfInterestEntity = await _cityInfoRepository
+          .GetCardAsync(id);
+
+      if (pointOfInterestEntity == null)
+        {
+        return NotFound();
+        }
+
+      _cityInfoRepository.DeleteCard(pointOfInterestEntity);
+      await _cityInfoRepository.SaveChangesAsync();
+
+      return NoContent();
+      }
+
+    [HttpPost]
+    public async Task<ActionResult<CardForCreationDto>> CreateCard(
+      CardForCreationDto pointOfInterest)
+      {
+      var finalPointOfInterest = _mapper.Map<Entities.Card>(pointOfInterest);
+      finalPointOfInterest.UserId = GetUserId();
+
+       _cityInfoRepository.AddCard(finalPointOfInterest);
+
+      await _cityInfoRepository.SaveChangesAsync();
+
+      var cardId = await _cityInfoRepository.GetMaxCardIdAsync();
+
+      var createdPointOfInterestToReturn =
+          _mapper.Map<Models.CardDto>(finalPointOfInterest);
+
+      return CreatedAtRoute("GetCard", new { id = cardId }, createdPointOfInterestToReturn);
+      }
+
+    private int GetUserId()
+      {
+      return Convert.ToInt32(User.Claims.ToList().FirstOrDefault().Value);
+      }
+
+    }
+}
