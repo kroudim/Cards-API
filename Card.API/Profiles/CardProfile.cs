03c8f44ed2e6bb90a0416e2c9843da44f4b62cd4
kroudim <kroudim@gmail.com>
2025-07-18 22:51:19 +0300

    Add files via upload

---
+++ Card.API/Profiles/CardProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace CityInfo.API.Profiles
+{
+    public class CardProfile : Profile
+    {
+        public CardProfile()
+        {
+            CreateMap<Entities.Card, Models.CardDto>();
+            CreateMap<Entities.Card, Models.CardForUpdateDto>();
+            CreateMap< Models.CardForUpdateDto, Entities.Card>();
+            CreateMap<Models.CardForCreationDto, Entities.Card>();
+      }
+    }
+}
